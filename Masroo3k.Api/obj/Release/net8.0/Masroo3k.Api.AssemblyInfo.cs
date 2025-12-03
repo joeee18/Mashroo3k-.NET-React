517d7d23b8b46e86260e5254fb1a71f417a4019e
joeee18 <youssefmagdy01004922751@gmail.com>
2025-12-03 18:02:38 +0200

    Initial project upload

--- Masroo3k.Api/obj/Release/net8.0/Masroo3k.Api.AssemblyInfo.cs
+++ Masroo3k.Api/obj/Release/net8.0/Masroo3k.Api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Masroo3k.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+99b17bdc4e1fa72782c555867f790960096397cb")]
 [assembly: System.Reflection.AssemblyProductAttribute("Masroo3k.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Masroo3k.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
